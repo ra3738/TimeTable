a9fe48442a538c6592512f56891cf3a245528c05
isthisevenreal <rithvik.al@gmail.com>
2019-09-13 21:24:08 -0300

    Scheduled all possible classes (including conflicts)

--- TimeTable/application.py
+++ TimeTable/application.py
@@ -6,136 +6,56 @@ from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
-course_name = ""
-course_number = ""
-
-# class_type = []
-# class_days = []
-# class_startTime = []
-# class_endTime = []
-
-lecture_days = []
-lecture_startTime = []
-lecture_endTime = []
-
-# list_days = []
-# list_startTime = []
-# list_endTime = []
-# list_courseNames = []
-
-firstSchedule_days = []
-firstSchedule_startTime = []
-firstSchedule_endTime = []
-
-secondSchedule_days = []
-secondSchedule_startTime = []
-secondSchedule_endTime = []
+# class_type, class_days, class_startTime, class_endTime = [], [], [], []
+course_name, course_number = "", ""
+lecture_days, lecture_startTime, lecture_endTime, lecture_courseNames = [], [], [], []
+firstSchedule_days, firstSchedule_startTime, firstSchedule_endTime = [], [], []
+secondSchedule_days, secondSchedule_startTime, secondSchedule_endTime = [], [], []
 
 def isThereConflict(i, j):
     return False
 
 def filterOut():
-    global firstSchedule_days
-    global firstSchedule_startTime
-    global firstSchedule_endTime
 
-    global secondSchedule_days
-    global secondSchedule_startTime
-    global secondSchedule_endTime
-
-    # if not firstSchedule_days:
-    #     firstSchedule_days = secondSchedule_days
-    #     firstSchedule_startTime = secondSchedule_startTime
-    #     firstSchedule_endTime = secondSchedule_endTime
-    #     print("firstSchedule_days: ")
-    #     print(firstSchedule_days)
-    #     return
+    global firstSchedule_days, firstSchedule_startTime, firstSchedule_endTime
+    global secondSchedule_days, secondSchedule_startTime, secondSchedule_endTime
 
     if not firstSchedule_days:
         for i in range(len(secondSchedule_days)):
             firstSchedule_days.append([secondSchedule_days[i]])
             firstSchedule_startTime.append([secondSchedule_startTime[i]])
             firstSchedule_endTime.append([secondSchedule_endTime[i]])
-            print("!!!!!!!!!!!!")
-
-        print(" ")
-        print("firstSchedule_days: ")
-        print(firstSchedule_days)
-        print(" ")
         return
 
-    temp_days = []
-    temp_startTime = []
-    temp_endTime = []
-
-    print("firstSchedule_days: ")
-    print(firstSchedule_days)
-    print("secondSchedule_days: ")
-    print(secondSchedule_days)
-    # print("secondSchedule_days: "+secondSchedule_days)
-
-    print(len(firstSchedule_days))
-    print(len(secondSchedule_days))
+    temp_days, temp_startTime, temp_endTime = [], [], []
 
     for i in range(len(firstSchedule_days)):
         for j in range(len(secondSchedule_days)):
-            # if not isThereConflict(i, j):
-            daysToAppend = firstSchedule_days[i].copy()
-            temp_days.append(daysToAppend)
-            startTimeToAppend = firstSchedule_startTime[i].copy()
-            temp_startTime.append(startTimeToAppend)
-            endTimeToAppend = firstSchedule_endTime.copy()
-            temp_endTime.append(endTimeToAppend)
-            index = len(temp_days)-1
-            temp_days[index].append(secondSchedule_days[j])
-            temp_startTime[index].append(secondSchedule_startTime[j])
-            temp_endTime[index].append(secondSchedule_endTime[j])
+            if not isThereConflict(i, j):
+                daysToAppend = firstSchedule_days[i].copy()
+                temp_days.append(daysToAppend)
+                startTimeToAppend = firstSchedule_startTime[i].copy()
+                temp_startTime.append(startTimeToAppend)
+                endTimeToAppend = firstSchedule_endTime[i].copy()
+                temp_endTime.append(endTimeToAppend)
+                index = len(temp_days)-1
+                temp_days[index].append(secondSchedule_days[j])
+                temp_startTime[index].append(secondSchedule_startTime[j])
+                temp_endTime[index].append(secondSchedule_endTime[j])
 
-            print(" ")
+            # print(" ")
+            # print(temp_startTime)
+            # print(" ")
             # print(temp_days)
-            # print(temp_startTime[index])
-            print(temp_startTime)
-            # print(secondSchedule_startTime[j])
-            # print(temp_endTime)
-
-    # for i in range(len(firstSchedule_days)):
-    #     for j in range(len(secondSchedule_days)):
-    #         if not isThereConflict(i, j):
-    #             print("Entered If")
-    #             temp_days.append([firstSchedule_days[i]])
-    #             temp_startTime.append([firstSchedule_startTime[i]])
-    #             temp_endTime.append([firstSchedule_endTime[i]])
-    #             index = len(temp_days)-1
-    #             temp_days[index].append(secondSchedule_days[j])
-    #             temp_startTime[index].append(secondSchedule_startTime[j])
-    #             temp_endTime[index].append(secondSchedule_endTime[j])
-    #
-    #             print(" ")
-    #             print(temp_days)
-    #             print(temp_startTime)
-    #             print(temp_endTime)
-    #             print(" ")
-    #
 
     firstSchedule_days = temp_days
-    # print("firstSchedule_days: "+firstSchedule_startTime)
     firstSchedule_startTime = temp_startTime
     firstSchedule_endTime = temp_endTime
 
 
-
 def parser():
-    global lecture_days
-    lecture_days = []
-    global lecture_startTime
-    lecture_startTime = []
-    global lecture_endTime
-    lecture_endTime = []
-
-    # global list_days
-    # global list_startTime
-    # global list_endTime
-    # global list_courseNames
+    global lecture_days, lecture_startTime, lecture_endTime, lecture_courseNames
+    lecture_days, lecture_startTime, lecture_endTime = [], [], []
 
     url = "https://courses.students.ubc.ca/cs/courseschedule?pname=subjarea&tname=subj-course&dept=" + course_name + "&course=" + course_number
     response = requests.get(url)
@@ -145,30 +65,17 @@ def parser():
 
     for i in range(len(section1)):
         section1_tdList = section1[i].findAll('td')
-        # class_type.append(section1_tdList[2].getText())
-        # class_days.append(section1_tdList[5].getText().split())
-        # class_startTime.append(section1_tdList[6].getText())
-        # class_endTime.append(section1_tdList[7].getText())
-
         if section1_tdList[2].getText() == "Lecture":
             lecture_days.append(section1_tdList[5].getText().split())
             lecture_startTime.append(section1_tdList[6].getText())
             lecture_endTime.append(section1_tdList[7].getText())
 
-    global secondSchedule_days
-    global secondSchedule_startTime
-    global secondSchedule_endTime
-
+    global secondSchedule_days, secondSchedule_startTime, secondSchedule_endTime
     secondSchedule_days = lecture_days
     secondSchedule_startTime = lecture_startTime
     secondSchedule_endTime = lecture_endTime
+    lecture_courseNames.append(course_name+" "+course_number)
 
-    print("ABC")
-
-    # list_days.append(lecture_days)
-    # list_startTime.append(lecture_startTime)
-    # list_endTime.append(lecture_endTime)
-    # list_courseNames.append(course_name+" "+course_number)
 
 @app.route("/")
 def index():
@@ -176,17 +83,32 @@ def index():
 
 @app.route("/table", methods=["POST"])
 def table():
-    global course_name
+    global course_name, course_number
     course_name = request.form.get("course_name")
-    global course_number
     course_number = request.form.get("course_number")
+
+    # global firstSchedule_days, firstSchedule_startTime, firstSchedule_endTime, lecture_courseNames
 
     parser()
     filterOut()
-    # lecture_days = [["Mon", "Wed", "Fri"], ["Mon", "Wed", "Fri"], ["Tue", "Thu"]]
-    # lecture_startTime = ["11:00", "13:00", "15:00"]
-    # lecture_endTime = ["12:00", "13:30", "16:30"]
 
-    return jsonify({"lecture_days": lecture_days, "lecture_startTime": lecture_startTime, "lecture_endTime": lecture_endTime})
+    print("temp_days: ")
+    print(firstSchedule_days)
+    print("temp_startTime: ")
+    print(firstSchedule_startTime)
+    print("temp_endTime: ")
+    print(firstSchedule_endTime)
 
-    # return jsonify({"success": True})
+    #
+    #
+    # firstSchedule_days = [[['Tue', 'Thu'], ['Tue', 'Thu']], [['Tue', 'Thu'], ['Tue', 'Thu']], [['Tue', 'Thu'], ['Tue', 'Thu']], [['Tue', 'Thu'], ['Mon', 'Wed', 'Fri']], [['Tue', 'Thu'], ['Mon', 'Wed', 'Fri']], [['Mon', 'Wed', 'Fri'], ['Tue', 'Thu']], [['Mon', 'Wed', 'Fri'], ['Tue', 'Thu']], [['Mon', 'Wed', 'Fri'], ['Tue', 'Thu']], [['Mon', 'Wed', 'Fri'], ['Mon', 'Wed', 'Fri']], [['Mon', 'Wed', 'Fri'], ['Mon', 'Wed', 'Fri']], [['Mon', 'Wed', 'Fri'], ['Tue', 'Thu']], [['Mon', 'Wed', 'Fri'], ['Tue', 'Thu']], [['Mon', 'Wed', 'Fri'], ['Tue', 'Thu']], [['Mon', 'Wed', 'Fri'], ['Mon', 'Wed', 'Fri']], [['Mon', 'Wed', 'Fri'], ['Mon', 'Wed', 'Fri']]]
+    #
+    # firstSchedule_startTime = [['12:30', '17:00'], ['12:30', '15:30'], ['12:30', '9:30'], ['12:30', '13:00'], ['12:30', '16:00'], ['15:00', '17:00'], ['15:00', '15:30'], ['15:00', '9:30'], ['15:00', '13:00'], ['15:00', '16:00'], ['11:00', '17:00'], ['11:00', '15:30'], ['11:00', '9:30'], ['11:00', '13:00'], ['11:00', '16:00']]
+    #
+    # firstSchedule_endTime = [['14:00', '18:30'], ['14:00', '17:00'], ['14:00', '11:00'], ['14:00', '14:00'], ['14:00', '17:00'], ['16:00', '18:30'], ['16:00', '17:00'], ['16:00', '11:00'], ['16:00', '14:00'], ['16:00', '17:00'], ['12:00', '18:30'], ['12:00', '17:00'], ['12:00', '11:00'], ['12:00', '14:00'], ['12:00', '17:00']]
+    #
+    # lecture_courseNames = ["CPSC 110", "CPSC 121"]
+
+    return jsonify({"firstSchedule_days": firstSchedule_days, "firstSchedule_startTime": firstSchedule_startTime,
+                    "firstSchedule_endTime": firstSchedule_endTime, "lecture_courseNames": lecture_courseNames})
+    # return jsonify({"lecture_days": lecture_days, "lecture_startTime": lecture_startTime, "lecture_endTime": lecture_endTime})
--- TimeTable/static/index.js
+++ TimeTable/static/index.js
@@ -50,9 +50,15 @@ reverseMap.set(20, "19:00");
 reverseMap.set(21, "19:30");
 reverseMap.set(22, "20:00")
 
-var lecture_days = [];
-var lecture_startTime = [];
-var lecture_endTime = [];
+// var lecture_days = [];
+// var lecture_startTime = [];
+// var lecture_endTime = [];
+
+var firstSchedule_days = [];
+var firstSchedule_startTime = [];
+var firstSchedule_endTime = [];
+var lecture_courseNames = []
+
 var timeDifference;
 var baseCell;
 var removedElements = [];
@@ -62,30 +68,56 @@ var course_name = "";
 var course_number = "";
 
 function color(){
-  for (i = 0; i < lecture_days[timetable_number].length; i++){
-    var timing = document.getElementById(lecture_startTime[timetable_number]);
-    var displayClass = "."+lecture_days[timetable_number][i];
-    timeDifference = lengthOfTimeDiff();
-    var cell = timing.querySelector(displayClass);
-    cell.style.backgroundColor= "lightgrey";
-    cell.rowSpan = timeDifference;
-    cell.innerHTML = course_name + " " + course_number;
-    removeElements(i);
+  for (i = 0; i < firstSchedule_days[timetable_number].length; i++){
+    for (j = 0; j < firstSchedule_days[timetable_number][i].length; j++){
+      // console.log("");
+      // console.log("Line 73");
+      var timing = document.getElementById(firstSchedule_startTime[timetable_number][i]);
+      // console.log("Line 75");
+      var displayClass = "."+firstSchedule_days[timetable_number][i][j];
+      // console.log("Line 77");
+      timeDifference = lengthOfTimeDiff(i);
+      var cell = timing.querySelector(displayClass);
+      // console.log("Line 80");
+      // console.log(firstSchedule_startTime[timetable_number][i]);
+      cell.style.backgroundColor= "lightgrey";
+      cell.rowSpan = timeDifference;
+      cell.innerHTML = lecture_courseNames[i];
+      removeElements(i, j);
+      // console.log("Line 85");
+
+
+      // console.log("!!!!!!");
+      // console.log(firstSchedule_days[timetable_number][i]);
+      // console.log(firstSchedule_days[timetable_number][i][j]);
+      // console.log(firstSchedule_startTime[timetable_number][i]);
+      // console.log(firstSchedule_endTime[timetable_number][i]);
+      // console.log("!!!!!!");
+    }
   }
 }
 
 // Assumes all inputs are valid and no overlapping classes
-function removeElements(i){
-  var timing = lecture_startTime[timetable_number];
+function removeElements(i, j){
+  // console.log("Line 101");
+  var timing = firstSchedule_startTime[timetable_number][i];
   var timeDiff = timeDifference-1;
+  // console.log("Line 104");
   while(timeDiff > 0){
     var common_time = map.get(timing);
     common_time = common_time + timeDiff;
+    // console.log("Line 108");
     var timing2 = document.getElementById(reverseMap.get(common_time));
-    var displayClass = "."+lecture_days[timetable_number][i];
+    // console.log("Line 110");
+    // console.log(firstSchedule_days[timetable_number][i][j]);
+    var displayClass = "."+firstSchedule_days[timetable_number][i][j];
+    // console.log("Line 112");
     var cell = timing2.querySelector(displayClass);
+    // console.log("Line 114");
+    // console.log(cell);
     cell.remove(cell);
     timeDiff--;
+    // console.log("Line 117");
   }
 }
 
@@ -98,9 +130,9 @@ function decolor(){
   start_.appendChild(table_1);
 }
 
-function lengthOfTimeDiff(){
-  startTime = map.get(lecture_startTime[timetable_number]);
-  endTime = map.get(lecture_endTime[timetable_number]);
+function lengthOfTimeDiff(i){
+  startTime = map.get(firstSchedule_startTime[timetable_number][i]);
+  endTime = map.get(firstSchedule_endTime[timetable_number][i]);
   return endTime-startTime;
 }
 
@@ -122,17 +154,23 @@ document.addEventListener('DOMContentLoaded', () => {
     request.onload = () => {
         // Extract JSON data from request
         const data = JSON.parse(request.responseText);
-        lecture_days = data.lecture_days;
-        lecture_startTime = data.lecture_startTime;
-        lecture_endTime = data.lecture_endTime;
+        // lecture_days = data.lecture_days;
+        // lecture_startTime = data.lecture_startTime;
+        // lecture_endTime = data.lecture_endTime;
+        //
+        // console.log(lecture_days);
+        // console.log(lecture_startTime);
+        // console.log(lecture_endTime);
 
-        console.log(lecture_startTime);
+        firstSchedule_days = data.firstSchedule_days;
+        firstSchedule_startTime = data.firstSchedule_startTime;
+        firstSchedule_endTime = data.firstSchedule_endTime;
+        lecture_courseNames = data.lecture_courseNames;
+
         var original_table = document.getElementById('table');
         table_1 = original_table.cloneNode(true);
         table_2 = original_table.cloneNode(true);
         console.log(table_1);
-
-        color();
     }
 
     const data = new FormData();
@@ -145,6 +183,12 @@ document.addEventListener('DOMContentLoaded', () => {
   };
 });
 
+
+document.addEventListener('DOMContentLoaded', () => {
+  document.querySelector('#submitCourses').onclick = () => {
+    color();
+  };
+});
 
 document.addEventListener('DOMContentLoaded', () => {
   document.querySelector('#next').onclick = () => {
